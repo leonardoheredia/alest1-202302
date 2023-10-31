ddf330376ab8e3461033d86da5ce6f287a8298d1
leonardoheredia <leonardo@heredia.com.br>
2023-10-30 22:07:59 -0300

    Aula 25 - ABP inclusao

---
+++ aula25_arvore_binaria_pesquisa/AppArvoreBinariaPesquisa.java
@@ -0,0 +1,11 @@
+package aula25_arvore_binaria_pesquisa;
+
+public class AppArvoreBinariaPesquisa {
+    public static void main(String[] args) {
+        ArvoreBinariaPesquisa abp = new ArvoreBinariaPesquisa();
+        abp.adicionar(60);
+        abp.adicionar(32);
+        abp.adicionar(15);
+        abp.adicionar(70);
+    }
+}
